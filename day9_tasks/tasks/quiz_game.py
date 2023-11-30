566349a0ac584a301e3e0bf4c5530ce3f20af23f
iamisha <isha.201710@ncit.edu.np>
2023-11-30 08:47:45 -0500

    quiz_game code updated

--- day9_tasks/tasks/quiz_game.py
+++ day9_tasks/tasks/quiz_game.py
@@ -1,12 +1,4 @@
-""" 
-3. Develop a quiz game where questions are stored in a JSON file. Use classes to
-represent the game, questions, and players. Functions can handle game logic, scoring,
-and displaying questions. The JSON file can store the questions with options and
-correct answers.
-"""
-
 import json
-
 
 class Question:
     def __init__(self, question_text, options, correct_answer):
@@ -14,12 +6,10 @@ class Question:
         self.options = options
         self.correct_answer = correct_answer
 
-
 class Player:
     def __init__(self, name):
         self.name = name
         self.score = 0
-
 
 class QuizGame:
     def __init__(self, questions_file):
@@ -29,22 +19,27 @@ class QuizGame:
     def play_game(self, player):
         for question in self.questions:
             self.show_question(question)
-            answer = input("Enter your answer: ")
-            if answer == question['correct_answer']:
-                player.score += 1
-                print("Correct!")
+            answer = input("Enter your answer (1, 2, 3, 4): ")
+
+            # Convert answer to an integer for comparison
+            answer = int(answer) if answer.isdigit() else None
+
+            if answer is not None and 1 <= answer <= len(question['options']):
+                if question['options'][answer - 1] == question['correct_answer']:
+                    player.score += 1
+                    print("Correct!")
+                else:
+                    print("Incorrect. The correct answer is", question['correct_answer'])
             else:
-                print("Incorrect. The correct answer is", question['correct_answer'])
+                print("Invalid input. Please enter a valid option.")
 
         print("Your final score is:", player.score)
-
 
     def show_question(self, question):
         print("----------------------------------------------------------------")
         print(question['question_text'])
-        for option in question['options']:
-            print(option)
-
+        for i, option in enumerate(question['options'], start=1):
+            print(f"{i}. {option}")
 
 if __name__ == "__main__":
     player_name = input("Enter your name: ")
